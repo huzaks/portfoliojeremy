e597c23d1d45e20de2cf5523da694e46ae7aed36
huzaks <jeremylavalleegiroux@gmail.com>
2023-05-16 19:48:35 -0400

    message box

--- components/Projets.jsx
+++ components/Projets.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 export default function Projets({ h3, src, alt, desc }) {
     return <div className={styles.cont}>
         <h3 className={styles.h3}>{h3}</h3>
+        <p className={styles.mobiledesc}>{desc}</p>
         <Image src={src} className={styles.image} alt={alt}></Image>
         <div className={styles.paragraphe_droite}>
             <h3 className={styles.titre}>{h3}</h3>
